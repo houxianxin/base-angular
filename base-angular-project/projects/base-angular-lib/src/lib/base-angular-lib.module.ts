6dc03df50e64731c18bf41c5b1ae0ea578477c51
houxianxin <houxianxin051@gmail.com>
2018-11-01 15:21:28 +0800

    add  dependencies

--- base-angular-project/projects/base-angular-lib/src/lib/base-angular-lib.module.ts
+++ base-angular-project/projects/base-angular-lib/src/lib/base-angular-lib.module.ts
@@ -1,13 +1,17 @@
 import {NgModule} from '@angular/core';
 import {BaseAngularLibComponent} from './base-angular-lib.component';
 import {HttpClientModule} from '@angular/common/http';
+import {TranslateModule} from '@ngx-translate/core';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
   imports: [
-    HttpClientModule
+    HttpClientModule,
+    TranslateModule,
+    FlexLayoutModule
   ],
   declarations: [BaseAngularLibComponent],
-  exports: [BaseAngularLibComponent]
+  exports: [BaseAngularLibComponent, TranslateModule, FlexLayoutModule]
 })
 export class BaseAngularLibModule {
 }
