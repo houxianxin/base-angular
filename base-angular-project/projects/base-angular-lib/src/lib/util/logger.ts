7da7acf01bed24592dcb356dc719038599be39ef
houxianxin <houxianxin051@gmail.com>
2018-11-02 09:54:24 +0800

    update

--- base-angular-project/projects/base-angular-lib/src/lib/util/logger.ts
+++ base-angular-project/projects/base-angular-lib/src/lib/util/logger.ts
@@ -3,10 +3,43 @@ import {BaseService} from '../service/base-service';
 
 export class Logger extends BaseService {
 
-  log(tag: string, message: any, ...optionalParams: any[]) {
+  v(tag: string, message: string) {
     if (isDevMode()) {
-      console.log(`[ ${tag} ] ${message}`, optionalParams);
+      console.log(`V [ ${tag} ] ${message}`);
     }
   }
 
+  i(tag: string, message: string) {
+    if (isDevMode()) {
+      console.log(`I [ ${tag} ] ${message}`);
+    }
+  }
+
+  d(tag: string, message: string) {
+    if (isDevMode()) {
+      console.log(`D [ ${tag} ] ${message}`);
+    }
+  }
+
+  w(tag: string, message: string) {
+    if (isDevMode()) {
+      console.warn(`W [ ${tag} ] ${message}`);
+    }
+  }
+
+  e(tag: string, message: string) {
+    if (isDevMode()) {
+      console.error(`E [ ${tag} ] ${message}`);
+    }
+  }
+
+  printObj(tag: string, obj: any) {
+    if (isDevMode()) {
+      try {
+        this.d(`${tag} printObj`, JSON.stringify(obj, null, 2));
+      } catch (err: any) {
+        this.e(`${tag} printObj`, err.toString());
+      }
+    }
+  }
 }
